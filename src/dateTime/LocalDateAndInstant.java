a75f330922e273a86380f4b50115ce8094f72a32
sypatrick <booth20037@gmail.com>
2023-01-18 23:43:39 +0900

    Date , LocalDate, Instant

---
+++ src/dateTime/LocalDateAndInstant.java
@@ -0,0 +1,12 @@
+package dateTime;
+
+import java.time.Instant;
+import java.util.Date;
+
+public class LocalDateAndInstant {
+    public static void main(String[] args) {
+        Date date = new Date();
+
+        Instant now = Instant.now();
+    }
+}
